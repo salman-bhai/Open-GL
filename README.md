97193dd4c1d84f5a3c555a3706a9385f43051333
Salman Shah <sbs.191197@gmail.com>
2017-02-07 11:58:09 +0530

    Instructions for Ubuntu updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Open-GL
 OpenGL Experiments and Algorithms
+
+
+## Compiling and Running the Programs
+* Type in the following command to compile and run your file
+```bash
+g++ <filename>.c -lGL -lGLU -lglut -o <filename>
+./<filename>
+```
---
+++ Source_Code/dda.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <math.h>
+#include <Windows.h>
+#include <GL/glut.h>
+
+double X1,Y1,X2,Y2;
+
+float round_value(float v) {
+    return floor(v+0.5);
+}
+
+void LineDDA(void) {
+    double dx = X2-X1;
+    double dy = Y2-Y1;
+    double steps;
+    if(abs(dx)>abs(dy))
+        steps=abs(dx);
+    else
+        steps=abs(dy);
+    float xInc,yInc;
+    float x=X1;
+    float y=Y1;
+    xInc=dx/(float)steps;
+    yInc=dy/(float)steps;
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3d(0,1,1);
+
+    glBegin(GL_POINTS);
+    glVertex2d(x,y);
+    int k;
+    for(k=0;k<steps;k++) {
+        x+=xInc;
+        y+=yInc;
+
+        glVertex2d(round_value(x),round_value(y));
+    }
+    glEnd();
+
+    glFlush();
+}
+
+void Init() {
+    glClearColor(0.0,0.0,1.0,0);
+    glColor3f(0.0,0.0,0.0);
+    gluOrtho2D(0,640,0,480);
+}
+
+int main(int argc, char *argv[]) {
+    printf("Enter two end points of the line to be drawn\n");
+    printf("\nEnter POINT 1(X1,Y1): ");
+    scanf("%lf%lf",&X1,&Y1);
+    printf("\nEnter POINT 2(X2,Y2): ");
+    scanf("%lf%lf",&X2,&Y2);
+
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(600,480);
+    glutCreateWindow("DDA_Line");
+    Init();
+    glutDisplayFunc(LineDDA);
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
+
